77d87335ed75ddf464d7f94aefbddda5efc98739
BuchiProf <46644031+BuchiProf@users.noreply.github.com>
2022-06-08 09:11:31 +0200

    Update install.sh
    ajout de la configuration basique du broker

--- 1_test_MQTT_mosquitto_rbpi/install.sh
+++ 1_test_MQTT_mosquitto_rbpi/install.sh
@@ -12,3 +12,18 @@ sudo apt-get install mosquitto-clients
 # on en profite pour inclure la bibliothèque python
 sudo apt-get install python3-pip
 sudo pip install paho-mqtt
+
+
+#on configure notre broker pour fonctionner sans identification ni mot de passe
+#de même on authorise les connexions anonymes
+
+#edition du fichier de config
+sudo nano /etc/mosquitto/mosquitto.conf
+#dans le fichier on rajoute (les deux lignes suivantes) le port du broker ainsi que les connexions anonymes
+listener 1883
+allow_anonymous true
+
+#on sauvegarde et on ferme l'éditeur : ctrl + x puis "oui"
+
+#on redémarre tout
+sudo reboot
